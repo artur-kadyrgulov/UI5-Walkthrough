e7464e032bfcf201326dfa2064d29d0edd5e7f8c
Artur Kadyrgulov <arturkadyrgulov@gmail.com>
2023-11-05 01:37:02 +0300

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+ ], (Controller, MessageToast) => {
     "use strict";
  
     return Controller.extend("ui5.walkthrough.controller.App", {
-       onShowHello() {
-          // show a native JavaScript alert
-          alert("Hello World");
-       }
+        onShowHello() {
+            MessageToast.show("This message should appear in the message toast", {
+                duration: 3000,                  // default
+                width: "15em",                   // default
+                my: "center bottom",             // default
+                at: "center bottom",             // default
+                of: window,                      // default
+                offset: "0 0",                   // default
+                collision: "fit fit",            // default
+                onClose: null,                   // default
+                autoClose: true,                 // default
+                animationTimingFunction: "ease", // default
+                animationDuration: 1000,         // default
+                closeOnBrowserNavigation: true   // default
+            });
+        }
     });
  });

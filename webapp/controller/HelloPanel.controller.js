3c28942331270181bb90766200944aff9e042f81
Artur Kadyrgulov <arturkadyrgulov@gmail.com>
2023-11-05 13:32:50 +0300

    Step 16: Dialogs and Fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -5,14 +5,21 @@ sap.ui.define([
     "use strict";
  
     return Controller.extend("ui5.walkthrough.controller.HelloPanel", {
-       onShowHello() {
-          // read msg from i18n model
-          const oBundle = this.getView().getModel("i18n").getResourceBundle();
-          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-          const sMsg = oBundle.getText("helloMsg", [sRecipient]);
- 
-          // show message
-          MessageToast.show(sMsg);
+        onShowHello() {
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+    
+            // show message
+            MessageToast.show(sMsg);
+        },
+
+        onOpenDialog() {
+            this.pDialog ??= this.loadFragment({
+                name: "ui5.walkthrough.view.HelloDialog"
+            });
+            this.pDialog.then((oDialog) => oDialog.open());
        }
     });
  });
